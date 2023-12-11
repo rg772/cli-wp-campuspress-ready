c1def188d67bccae9c23eac46d500e6cf8ffbcba
r <r@>
2023-12-11 15:51:11 -0600

    network execution

---
+++ README.md
@@ -0,0 +1,4 @@
+To use, copy-paste the following into the wordpress plugins folder
+````
+wget -O - https://raw.githubusercontent.com/rg772/cli-wp-campuspress-ready/main/run.py | python3
+``````
--- run.py
+++ run.py
@@ -5,15 +5,7 @@ import os
 # URL of the file
 URL = "https://raw.githubusercontent.com/rg772/cli-wp-campuspress-ready/main/plugins.dat"
 
-"""
-This function sends a GET request to the specified URL and returns the response text as an array of lines.
 
-Args:
-    URL (str): The URL to send the GET request to.
-
-Returns:
-    list: A list of lines in the response text, or None if the request was unsuccessful.
-"""
 def load_url_into_array(URL):
     # Send a GET request to the URL
     response = requests.get(URL)
@@ -26,26 +18,13 @@ def load_url_into_array(URL):
         print(f"Failed to fetch the file. HTTP status code: {response.status_code}")
         return None
 
-"""
-This function checks if a directory exists and exits the program if it does not.
 
-Args:
-    path (str): The path of the directory to check.
-"""
 def check_folder_exists(path):
-     if not os.path.isdir(path):
+    if not os.path.isdir(path):
         print(f"Directory {path} does not exist.")
         sys.exit()
 
-"""
-This function scans a directory and returns all containing directories in a list.
 
-Args:
-    path (str): The path of the directory to scan.
-
-Returns:
-    list: A list of directory names in the specified path.
-"""
 def get_directories(path):
     return [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
 
@@ -62,3 +41,9 @@ plugins_expected = load_url_into_array()
 # Scan folder 
 folders_existing = get_directories(target_folder)
 
+# Use sets to compare
+plugins_expected = set(plugins_expected)
+folders_existing = set(folders_existing)
+set_diff = folders_existing - plugins_expected
+
+print(folders_existing)
